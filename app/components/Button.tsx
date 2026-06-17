2ab7586429e8c6afa32315f8e071d4e5317b6558
김유리 <uowllnh@gmail.com>
2026-06-18 02:28:15 +0900

    feat: 방명록과 사진 업로드 섹션 추가

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -16,7 +16,7 @@ export default function Button({
     <div className="flex">
       <button
         type={type}
-        className={`flex-1 min-w-44.5 px-10 mt-16 flex h-12 items-center justify-center gap-1.75 rounded-[11px] border-[0.5px] border-[#C9C9C9] bg-white text-[15px] text-black  ${className}`}
+        className={`flex-1 min-w-44.5 px-10 mt-16 flex h-12 items-center justify-center gap-1.75 rounded-[11px] border-[0.3px] border-[#C9C9C9] bg-white text-[15px] text-black  ${className}`}
         {...props}
       >
         {icon && (
